6806dd73ba8ff8658fc30606e68f63cfae1f661d
Evan Ray <evan.l.ray@gmail.com>
2018-02-12 11:56:10 -0500

    add rail_trail data from mosaicData, move lake_huron data to base_r folder

--- data/base_r/save-r-data.R
+++ data/base_r/save-r-data.R
@@ -5,3 +5,8 @@ write_csv(anscombe, "anscombe.csv")
 names(faithful) <- c("eruption_duration_min", "time_to_next_eruption_min")
 write_csv(faithful, "faithful.csv")
 
+LakeHuron <- data.frame(
+  year = seq(from = 1875, to = 1972),
+  water_level = LakeHuron
+)
+write_csv(LakeHuron, "lake_huron.csv")
--- data/mosaic/save-mosaic-data.R
+++ data/mosaic/save-mosaic-data.R
@@ -1,9 +1,4 @@
 library(mosaicData)
 library(readr)
 
-LakeHuron <- data.frame(
-  year = seq(from = 1875, to = 1972),
-  water_level = LakeHuron
-)
-
-write_csv(LakeHuron, "lake_huron.csv")
+write_csv(mosaicData::RailTrail, "data/mosaic/rail_trail.csv")
