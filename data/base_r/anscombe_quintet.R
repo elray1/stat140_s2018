1daa0b57796d9f007a0f091745e38c2576926ff0
Evan Ray <evan.l.ray@gmail.com>
2018-04-16 14:48:31 -0400

    heteroskedastic anscombe

--- data/base_r/anscombe_quintet.R
+++ data/base_r/anscombe_quintet.R
@@ -1,4 +1,5 @@
 library(readr)
+library(dplyr)
 
 set.seed(1)
 anscombe$x5 <- anscombe$x1
@@ -20,5 +21,10 @@ summary(lm(anscombe$y5 ~ anscombe$x5))
 #plot(anscombe$y5 ~ anscombe$x5)
 
 ggplot(data = anscombe, mapping = aes(x = x5, y = y5)) + geom_point() + geom_smooth(method = "lm", se = FALSE)
+
+anscombe <- anscombe %>%
+  select(
+    x1, x2, x3, x4, x5, y1, y2, y3, y4, y5
+  )
 
 write_csv(anscombe, "anscombe_quintet.csv")
